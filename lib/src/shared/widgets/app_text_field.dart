375e19cf9e543e7757fe254aa80c41015c298ae7
KbDevs12 <daniaku203@gmail.com>
2026-05-15 00:30:17 +0700

    chore: edit text field

--- lib/src/shared/widgets/app_text_field.dart
+++ lib/src/shared/widgets/app_text_field.dart
@@ -5,20 +5,26 @@ class AppTextField extends StatelessWidget {
     required this.controller,
     required this.label,
     this.hint,
+    this.prefixIcon,
+    this.suffixIcon,
     this.keyboardType,
     this.obscureText = false,
     this.textInputAction,
     this.validator,
+    this.maxLines = 1,
     super.key,
   });
 
   final TextEditingController controller;
   final String label;
   final String? hint;
+  final IconData? prefixIcon;
+  final Widget? suffixIcon;
   final TextInputType? keyboardType;
   final bool obscureText;
   final TextInputAction? textInputAction;
   final String? Function(String?)? validator;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
@@ -28,9 +34,12 @@ class AppTextField extends StatelessWidget {
       obscureText: obscureText,
       textInputAction: textInputAction,
       validator: validator,
+      maxLines: obscureText ? 1 : maxLines,
       decoration: InputDecoration(
         labelText: label,
         hintText: hint,
+        prefixIcon: prefixIcon == null ? null : Icon(prefixIcon),
+        suffixIcon: suffixIcon,
       ),
     );
   }
