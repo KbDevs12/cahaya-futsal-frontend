71add27a57d1fc182a18bc53defdfffabdc1a9dd
KbDevs12 <daniaku203@gmail.com>
2026-05-15 00:31:48 +0700

    chore: editing snackbar

--- lib/src/core/utils/snackbar.dart
+++ lib/src/core/utils/snackbar.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-void showSnack(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(
+void showSnack(BuildContext context, String message, {bool isError = false}) {
+  final messenger = ScaffoldMessenger.of(context);
+  messenger.hideCurrentSnackBar();
+  messenger.showSnackBar(
     SnackBar(
-      behavior: SnackBarBehavior.floating,
       content: Text(message),
+      backgroundColor: isError ? Theme.of(context).colorScheme.error : null,
     ),
   );
 }
