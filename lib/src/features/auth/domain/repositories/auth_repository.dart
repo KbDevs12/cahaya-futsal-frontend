a1ee8e6b0ac565e728b7f2da15a964b53ddbb0ed
KbDevs12 <daniaku203@gmail.com>
2026-07-09 22:20:57 +0700

    feat: adding multi role system - registering

--- lib/src/features/auth/domain/repositories/auth_repository.dart
+++ lib/src/features/auth/domain/repositories/auth_repository.dart
@@ -3,7 +3,16 @@ import '../entities/auth_session.dart';
 abstract class AuthRepository {
   Future<AuthSession?> readSavedSession();
   Future<AuthSession> login({required String email, required String password});
-  Future<void> register({required String name, required String email, required String password});
+
+  Future<AuthSession> adminLogin({
+    required String email,
+    required String password,
+  });
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+  });
   Future<void> sendPasswordResetEmail(String email);
   Future<void> logout();
 }
