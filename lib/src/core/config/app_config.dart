f99aca00ec510e2a93947e24841ab86616a3f5b2
KbDevs12 <daniaku203@gmail.com>
2026-07-17 13:30:14 +0700

    feat: changing url for dev

--- lib/src/core/config/app_config.dart
+++ lib/src/core/config/app_config.dart
@@ -3,6 +3,6 @@ class AppConfig {
 
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://hunter-womanless-freeware.ngrok-free.dev/api/v1',
+    defaultValue: 'https://roulette-john-careless.ngrok-free.dev/api/v1',
   );
 }
